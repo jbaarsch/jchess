2f4854b721cf680463e502114c0d9a3ea5c7db50
Jon Baarsch <jdbaarsch@gmail.com>
2024-05-13 14:50:24 -0500

    start over

--- jchess/src/main/java/com/jchess/MoveRepository.java
+++
@@ -1,8 +0,0 @@
-package com.jchess;
-
-import org.springframework.data.mongodb.repository.*;
-
-
-public class MoveRepository //extends MongoRepository<User, Integer> {
-{
-}
--- jchess/src/main/java/com/jchess/User.java
+++
@@ -1,5 +0,0 @@
-package com.jchess;
-
-public class User {
-
-}
